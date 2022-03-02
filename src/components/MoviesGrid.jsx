6dbd3011b335bb1da114480c2c307587d54cb59a
maxi <maxiarieloliva@gmail.com>
2022-03-02 17:49:10 -0300

    parte 4 actualizada spin

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
-
+import { Spinner } from "./Spinner";
 import { get } from "../utils/httpClient";
 import { MovieCard } from "./MovieCard";
 import styles from "./MoviesGrid.module.css";
 import { useQuery } from "../Hooks/useQuery";
-import { Search } from "./Search";
 
 export function MoviesGrid() {
   const [movies, setMovies] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   const query = useQuery();
   const search = query.get("search");
 
   useEffect(() => {
+    setIsLoading(true);
     const searchUrl = search
       ? "/search/movie?query=" + search
       : "/discover/movie";
     get(searchUrl).then((data) => {
       setMovies(data.results);
+      setIsLoading(false);
     });
   }, [search]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
