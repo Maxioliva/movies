2874c0267ba4ecdd35501e98bea3fb7af6b273eb
John Doe <maxiarieloliva@gmail.com>
2022-06-22 13:06:33 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export function App() {
     <Router>
       <header>
         <Link to="/">
-          <h1 className={styles.title}>Movies</h1>
+          <h1 className={styles.title}>Movies Web</h1>
         </Link>
       </header>
       <main>
